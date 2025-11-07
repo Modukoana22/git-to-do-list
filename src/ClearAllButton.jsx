9c8d1f2f001eddee8e2acce7b198b878d814bba0
Modukoana <modukoana@drop-it.tech>
2025-11-07 11:28:02 +0200

    changed color to blue

--- src/ClearAllButton.jsx
+++ src/ClearAllButton.jsx
@@ -8,7 +8,7 @@ export default function ClearAllButton({ onClear, visible }) {
       onClick={onClear}
       style={{
         marginLeft: "1rem",
-        backgroundColor: "tomato",
+        backgroundColor: "blue",
         color: "white",
         border: "none",
         padding: "0.4rem 0.8rem",
